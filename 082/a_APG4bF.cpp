473faafb7a53421490c832c07be0ed804f0f970b
tomeda <127189919+tomeda-r@users.noreply.github.com>
2023-03-07 19:49:39 +0900

    Create a_APG4bF.cpp

---
+++ 082/a_APG4bF.cpp
@@ -0,0 +1,19 @@
+//23.03.07
+//APG4b Fまでの知識で作成
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a, b; //入力値
+    int c; //出力値
+    double m; //平均
+
+    cin >> a >> b;
+    m = (a+b)/2;
+    c = m + 0.9;
+
+    cout << c << endl;
+    
+}
